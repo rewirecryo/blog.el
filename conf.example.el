b71f5b52459d1660917f5e2e14272cd32518a1a3
RewireCryo <cryrasinia@proton.me>
2026-03-15 17:39:37 -0600

    [+] Se crea el script update-site, y un ejemplo de conf.el, cual configura update-site

---
+++ conf.example.el
@@ -0,0 +1,8 @@
+;; Path to the blog's database
+(setq database-path "blog.db")
+
+;; Path to the authors file
+(setq authors-file-path "authors.json")
+
+;; If blog.el is installed in a directory that's not in your load path, you can modify it here
+;; (add-to-list 'load-path "/path/to/blog.el/directory")
