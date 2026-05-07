18aec2248b0aa7965b29364a02d6ffb68878a30b
RewireCryo <cryrasinia@proton.me>
2026-05-07 09:31:12 -0600

    refactor(blog-author.el): Se quitan funciones inútiles

--- src/blog-author.el
+++ src/blog-author.el
@@ -11,31 +11,6 @@
    (avatar-sets :accessor blog-author-avatar-sets
 		:initarg :avatar-sets)))
 
-(defun blog-fetch-stored-authors-file-hash (db)
-  "Query database DB for the hash value for author.json that was last stored."
-  (let ((hash-row (nth 0 (sqlite-select db "SELECT hash FROM authors_file_hash_table WHERE id=1;"))))
-    (if hash-row
-	(nth 0 hash-row))))
-
-(defun blog-stored-authors-needs-update (db authors-file-object)
-  "Check whether the list of authors in database DB needs to be updated.
-
-It's considered out of date in the following cases:
-
-  1) The hash in DB is different from the hash of the author file AUTHOR-FILE.
-
-     In this case, the calculated hash of the author file is returned.
-
-  2) There is no hash stored in DB
-
-     In this case, t is returned."
-  (let ((stored-hash (blog-fetch-stored-authors-file-hash db)) (calculated-hash nil))
-    (if (not stored-hash)
-	t
-      (setq calculated-hash (blog-hash (blog-git-object-show authors-file-object)))
-      (if (not (string-equal stored-hash calculated-hash))
-	  calculated-hash)))) ;; Return the calculated hash if it's different from that in the database
-
 (defun blog-load-avatar-sets-from-array (j-avatar-set number-of-sizes)
   "Given an array from an author JSON object parsed from an author file,
 J-AVATAR-SET, return an alist that could be used as an
